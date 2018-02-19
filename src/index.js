b98288629ad43a3c50abc04fad009446e7627081
Alexey <cashto44my@yandex.ru>
2018-02-19 23:39:23 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function warmup(temperature) {
-  // your implementation here
+  //Check input data. If argument == NaN return ERRmsg 
+  if (isNaN(temperature)) return "Give me number please!";
+  //For insurance, convert argument to INT and apply formula to find result
+  return parseInt(temperature) * 9 / 5 + 32;
 };
